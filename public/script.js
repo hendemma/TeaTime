278c5f0484c44c4c98af2f772b21b6bd462bda18
Emma Henderson <hendemma@gmail.com>
2025-03-26 18:16:52 -0700

    Delete public directory

--- public/script.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-    const menu = document.getElementById("menuLinks");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    }
-    else {
-        menu.style.display = "block";
-    }
-}
